356f45f14abb03131e3cb43415ebe55c8bae3fc6
Lainthrill <adyyadyy@gmail.com>
2021-03-24 16:59:17 +0100

    could sort

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -4,17 +4,56 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ChessResults {
 
     public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+        Map<String, Integer> mapForList = new HashMap<>();
+
+        List<String> mostPoints = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                mapForList.put()
+            }
+            mostPoints = setOrder(mostPoints);
+        } catch (IOException e) {
+            System.out.printf("File not found!");
+        }
+        return mostPoints;
+
+//        List<String> mostPoints = new ArrayList<>();
+//        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                mostPoints.add(line);
+//            }
+//            mostPoints = setOrder(mostPoints);
+//        } catch (IOException e) {
+//            System.out.printf("File not found!");
+//        }
+//        return mostPoints;
+    }
+
+    public List<String> setOrder(List<String> list) {
+        List<String> namesWithTotalPoints = new ArrayList<>();
+        Map<String, Integer> mapForList = new HashMap<>();
+
+
+//        for (int i = 0; i < list.size(); i++) {
+//            int totalPoints = 0;
+//            for (int j = 0; j < list.get(i).length() ; j++) {//összeadja a pontokat
+//                char temp = list.get(i).charAt(j);
+//                if (Character.isDigit(temp)) {
+//                    totalPoints += temp;
+//                }
+//            }
+//        }
+
+
+
+        return Collections.emptyList();
     }
 
 }
