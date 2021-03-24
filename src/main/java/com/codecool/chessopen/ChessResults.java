bc7a4e1846ab1de1f2561e9d65cc99d4c9dd07bf
Lainthrill <adyyadyy@gmail.com>
2021-03-24 17:03:42 +0100

    couldn't sort

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,7 +1,6 @@
 package com.codecool.chessopen;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -14,45 +13,16 @@ public class ChessResults {
         List<String> mostPoints = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
-            while ((line = br.readLine()) != null) {
-                mapForList.put()
-            }
             mostPoints = setOrder(mostPoints);
         } catch (IOException e) {
             System.out.printf("File not found!");
         }
         return mostPoints;
-
-//        List<String> mostPoints = new ArrayList<>();
-//        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-//            String line;
-//            while ((line = br.readLine()) != null) {
-//                mostPoints.add(line);
-//            }
-//            mostPoints = setOrder(mostPoints);
-//        } catch (IOException e) {
-//            System.out.printf("File not found!");
-//        }
-//        return mostPoints;
     }
 
     public List<String> setOrder(List<String> list) {
         List<String> namesWithTotalPoints = new ArrayList<>();
         Map<String, Integer> mapForList = new HashMap<>();
-
-
-//        for (int i = 0; i < list.size(); i++) {
-//            int totalPoints = 0;
-//            for (int j = 0; j < list.get(i).length() ; j++) {//összeadja a pontokat
-//                char temp = list.get(i).charAt(j);
-//                if (Character.isDigit(temp)) {
-//                    totalPoints += temp;
-//                }
-//            }
-//        }
-
-
-
         return Collections.emptyList();
     }
 
